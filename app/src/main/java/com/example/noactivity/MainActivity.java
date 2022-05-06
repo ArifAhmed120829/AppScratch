71a58b29e81fd17e084d99d1fdf2d7382640f7c7
ArifAhmed <arif15-12301@diu.edu.bd>
2022-05-06 12:57:24 +0600

    Dispalying and saving text

--- app/src/main/java/com/example/noactivity/MainActivity.java
+++ app/src/main/java/com/example/noactivity/MainActivity.java
@@ -1,20 +1,44 @@
 package com.example.noactivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editext;
+    Button button;
+    TextView textView;
+
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout);
-        TextView txt = findViewById(R.id.text1);
-        TextView txt2 = findViewById(R.id.text2);
-        txt.setText("Hello World");
-        txt2.setText("Bye World");
+        setContentView(R.layout.hi);
 
+        editext = findViewById(R.id.edittext);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textview);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String et = editext.getText().toString();
+                displayandsavetext(et);
+
+            }
+        });
+
+
+    }
+    private void displayandsavetext(String enText){
+        textView.setText(enText);
+        SharedPreferences sharedPreferences = getSharedPreferences("mysharedpreferences",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("name",enText);
+        editor.commit();
 
     }
 
